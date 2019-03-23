3e2ac998395e37279ce528807c473f15d83e7f3a
Mhage <masahage@soleil.ocn.ne.jp>
2019-03-23 16:41:10 +0900

    refactoring

---
+++ M5StackFIRE_MMD/ARlikeMotion.cpp
@@ -0,0 +1,43 @@
+#include "ARlikeMotion.h"
+
+ARlikeMotion::ARlikeMotion() {
+  Wire.begin();
+  IMU.initMPU9250();
+}
+
+void ARlikeMotion::CreateMatrix(Matrix* matrix, Matrix* matrixR) {
+  Matrix matrixScale, matrixT, matrixTR, matrixTRT0, matrixT0;
+  float r[3][3] = {{*getR2(), *(getR2()+3), *(getR2()+6)}, {*(getR2()+1), *(getR2()+4), *(getR2()+7)}, {*(getR2()+2), *(getR2()+5), *(getR2()+8)}};
+  matrixR->m11 = +r[0][0]; matrixR->m12 = +r[0][2]; matrixR->m13 = +r[0][1]; matrixR->m14 = 0;
+  matrixR->m21 = -r[1][0]; matrixR->m22 = -r[1][2]; matrixR->m23 = -r[1][1]; matrixR->m24 = 0;
+  matrixR->m31 = +r[2][0]; matrixR->m32 = +r[2][2]; matrixR->m33 = +r[2][1]; matrixR->m34 = 0;
+  matrixR->m41 = 0; matrixR->m42 = 0; matrixR->m43 = 0; matrixR->m44 = 1;
+  Affine::CreateTranslationMatrix(120, 100, 0, &matrixT);   // centering for screen
+  Affine::CreateTranslationMatrix(0, -65, 0, &matrixT0);    // centering for rotation axis
+  Affine::CreateScalingMatrix(18, &matrixScale);
+  Affine::Dot(&matrixT, matrixR, &matrixTR);
+  Affine::Dot(&matrixTR, &matrixT0, &matrixTRT0);
+  Affine::Dot(&matrixTRT0, &matrixScale, matrix);
+}
+
+void ARlikeMotion::ReviseIMU() {
+ if (IMU.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
+    {  
+      IMU.readAccelData(IMU.accelCount);
+      IMU.getAres();
+
+      IMU.ax = (float)IMU.accelCount[0]*IMU.aRes;
+      IMU.ay = (float)IMU.accelCount[1]*IMU.aRes;
+      IMU.az = (float)IMU.accelCount[2]*IMU.aRes;
+
+      IMU.readGyroData(IMU.gyroCount);
+      IMU.getGres();
+
+      IMU.gx = (float)IMU.gyroCount[0]*IMU.gRes;
+      IMU.gy = (float)IMU.gyroCount[1]*IMU.gRes;
+      IMU.gz = (float)IMU.gyroCount[2]*IMU.gRes;
+    }
+    IMU.updateTime();
+    // Don't use mag since it's too noisy indoor
+    MadgwickQuaternionUpdateIMU2(IMU.ax, IMU.ay, IMU.az, IMU.gx*DEG_TO_RAD, IMU.gy*DEG_TO_RAD, IMU.gz*DEG_TO_RAD, IMU.deltat); 
+}
--- M5StackFIRE_MMD/M5StackFIRE_MMD.ino
+++ M5StackFIRE_MMD/M5StackFIRE_MMD.ino
@@ -1,83 +1,40 @@
 #include <M5Stack.h>
 #include "PixelShader.h"
-#include "MatrixFactory.h"
+#include "SimpleMotion.h"
 #include "BitmapFactory.h"
 #include "FpsMeter.h"
 #include "Mesh.h"
-#include "utility/MPU9250.h"
-#include "quaternionFilters2.h"
+#include "ARlikeMotion.h"
+#define ENABLE_ARLIKE // uncomment and disenable ARlikeMotion
 
 PixelShader* pixelShader;
 FpsMeter* fpsMeter;
 Mesh* mesh;
-MPU9250 IMU;
-
-void InitAR() {
-  Wire.begin();
-  byte c = IMU.readByte(MPU9250_ADDRESS, WHO_AM_I_MPU9250);
-  IMU.initMPU9250();
-}
-
-void CreateARMatrix(Matrix* matrix, Matrix* matrixR) {
-  Matrix matrixAR, matrixRx, matrixRy, matrixRz, matrixScale, matrixT, matrixTR, temp;
-  float r[3][3] = {{*getR2(), *(getR2()+3), *(getR2()+6)}, {*(getR2()+1), *(getR2()+4), *(getR2()+7)}, {*(getR2()+2), *(getR2()+5), *(getR2()+8)}};
-  // matrixR->m11 = +r[0][0]; matrixR->m12 = r[0][2]; matrixR->m13 = -r[0][1]; matrixR->m14 = 0;
-  // matrixR->m21 = -r[1][0]; matrixR->m22 = -r[1][2]; matrixR->m23 = -r[1][1]; matrixR->m24 = 0;
-  // matrixR->m31 = +r[2][0]; matrixR->m32 = r[2][2]; matrixR->m33 = +r[2][1]; matrixR->m34 = 0;
-  // matrixR->m41 = 0; matrixR->m42 = 0; matrixR->m43 = 0; matrixR->m44 = 1;
-
-  matrixAR.m11 = -r[0][0]; matrixAR.m12 = r[0][1]; matrixAR.m13 = r[0][2]; matrixAR.m14 = 0;
-  matrixAR.m21 = +r[1][0]; matrixAR.m22 = -r[1][1]; matrixAR.m23 = -r[1][2]; matrixAR.m24 = 0;
-  matrixAR.m31 = -r[2][0]; matrixAR.m32 = r[2][1]; matrixAR.m33 = r[2][2]; matrixAR.m34 = 0;
-  matrixAR.m41 = 0; matrixAR.m42 = 0; matrixAR.m43 = 0; matrixAR.m44 = 1;
-  Affine::CreateRotationMatrixX(PI/2, &matrixRx);
-  Affine::CreateRotationMatrixZ(PI, &matrixRz);
-  Affine::Dot(&matrixRz, &matrixRx, &temp);
-  Affine::Dot(&matrixAR, &temp, matrixR);
-  Affine::CreateTranslationMatrix(120, 100, 0, &matrixT);
-  Affine::CreateScalingMatrix(18, &matrixScale);
-  Affine::Dot(&matrixT, matrixR, &matrixTR);
-  Affine::Dot(&matrixTR, &matrixScale, matrix);
-}
-
-void ReviseIMU() {
- if (IMU.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
-    {  
-      IMU.readAccelData(IMU.accelCount);
-      IMU.getAres();
-
-      IMU.ax = (float)IMU.accelCount[0]*IMU.aRes;
-      IMU.ay = (float)IMU.accelCount[1]*IMU.aRes;
-      IMU.az = (float)IMU.accelCount[2]*IMU.aRes;
-
-      IMU.readGyroData(IMU.gyroCount);
-      IMU.getGres();
-
-      IMU.gx = (float)IMU.gyroCount[0]*IMU.gRes;
-      IMU.gy = (float)IMU.gyroCount[1]*IMU.gRes;
-      IMU.gz = (float)IMU.gyroCount[2]*IMU.gRes;
-    }
-    IMU.updateTime();
-    // Don't use mag since it's too noisy indoor
-    MadgwickQuaternionUpdateIMU2(IMU.ax, IMU.ay, IMU.az, IMU.gx*DEG_TO_RAD, IMU.gy*DEG_TO_RAD, IMU.gz*DEG_TO_RAD, IMU.deltat); 
-}
+SimpleMotion* simpleMotion;
+ARlikeMotion* arlikeMotion;
 
 void setup(){
-  M5.begin();
+  M5.begin(); // This inludes Serial.begin(). Don't call it.
   delay(100);
-  // Serial.begin(115200); // Don't call it! M5.begin() includes it. If it is called again, some problems occcurs as freeze.
   pixelShader = new PixelShader();
   fpsMeter = new FpsMeter();
   mesh = new Mesh("/Shanghai.pmd");// File name should be ASCII
-  InitAR();
+#ifdef ENABLE_ARLIKE
+  arlikeMotion = new ARlikeMotion();
+#else
+  simpleMotion = new SimpleMotion();
+#endif
 }
 
 void loop() {
   static float t = 0.0f;
   Matrix matrix, matrixR;
-  //MatrixFactory::CreateMatrix(&matrix, &matrixR, t);
-  CreateARMatrix(&matrix, &matrixR);
-  ReviseIMU();
+#ifdef ENABLE_ARLIKE
+  arlikeMotion->CreateMatrix(&matrix, &matrixR);
+  arlikeMotion->ReviseIMU();
+#else
+  simpleMotion->CreateMatrix(&matrix, &matrixR, t);
+#endif
   for (int i = 0; i < mesh->numVertex; ++i) {
     Affine::Dot(&matrix, &mesh->pos0[i], &mesh->pos[i]);
     Affine::Dot(&matrixR, &mesh->normal0[i], &mesh->normal[i]);
